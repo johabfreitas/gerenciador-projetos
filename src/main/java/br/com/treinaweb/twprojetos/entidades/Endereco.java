041596c4ced58f1ca9b314ac20c30047e9b6baf3
Johab Freitas <freitasjohab@gmail.com>
2022-12-14 16:06:16 -0300

    Criando entidades

---
+++ src/main/java/br/com/treinaweb/twprojetos/entidades/Endereco.java
@@ -0,0 +1,11 @@
+package br.com.treinaweb.twprojetos.entidades;
+
+public class Endereco {
+
+	private Long id;
+
+	private String cidade;
+	private String logradouro;
+	private String numero;
+	private String complemeto;
+}
